d3651f0f72821a1a4f4b64c68382d4abbab870a0
Rachel Self <rachel.m.self@gmail.com>
2014-09-24 16:20:39 -0500

    Add config.fog_directory with S3 bucket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,13 +6,14 @@ CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = true
   else
+    config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],        # required
       :aws_secret_access_key  => ENV['AWS_ACCESS_SECRET'],     # required
     }
-    config.storage = :fog
+
     config.fog_public     = false                                   # optional, defaults to true
-    config.fog_directory  = 'name_of_directory'                     # required
+    config.fog_directory  = 'artlab'                     # required
   end
 end
