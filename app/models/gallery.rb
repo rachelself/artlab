4a1a2fb708cbec817fa99e86272831ead46140cc
Rachel Self <rachel.m.self@gmail.com>
2014-09-25 14:30:46 -0500

    Refactor galleries#new form to require title

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,7 +3,7 @@ class Gallery < ActiveRecord::Base
   has_many :photos
 
   validates_presence_of :title
-  validates_length_of :title, maximum: 30, message: "30 is the maximum allowed", allow_blank: true
+  validates_length_of :title, maximum: 30, message: "30 is the maximum allowed", allow_blank: false
   validates_format_of :title, :description, with: /[a-zA-Z]/, message: "must include letters", allow_blank: true
   validates_length_of :description, maximum: 300, message: "300 is the maximum allowed", allow_blank: true
 end
